82a3932d30367e69c0d1e39ee2f98fce7e7b62bb
Kai Iwasaki <kame.kai123@gmail.com>
2021-01-27 18:55:07 -0800

    trying to commit seppuku

---
+++ Project4/Project4.cpp
@@ -0,0 +1,34 @@
+/*
+Name: Kai Iwasaki
+Class: CPSC 122
+Date Submitted: January 27, 2021
+Assignment: Project 4
+Description: This project does stuff
+*/
+
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	ofstream fout;			// declare output file
+	fout.open(argv[1]);		// match out file as first argument in call
+	
+	int x, y;
+	cout << "How many Prime Numeros, Señor? ";
+	cin >> x;
+	
+	cout << '\n' << "How many columns would you like it to be printed in, Señor? ";
+	cin >> y;
+	
+/*	int[] primes = int[x];
+	
+	int currentnumber = 2;
+	for (int i = 0; i < x; i++){
+		while (currentnumber%
+	} 
+	
+	fout << "hi" << '\n';
+*/		
+}
